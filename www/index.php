499f2d4076de6f6d2af2a397aa8d323e9f03f102
Demian Turner <demian@phpkitchen.com>
2010-05-13 18:21:48 +0100

    fixed autoloader to correctly consider include_path

--- www/index.php
+++ www/index.php
@@ -8,11 +8,17 @@ $sglLibDir = $root .'/sgl2/src/lib';
 
 require $sglLibDir.'/Uber.php';
 Uber::init();
+Uber_Loader::addAutoloadPattern(array(
+    'preg_replace'=> array(
+        '_'=>'/'),
+        'suffix'=>'.php',
+        'basedir'=>get_include_path()));
 Uber_Loader::registerNamespace('Uber', $sglLibDir);
 Uber_Loader::registerNamespace('SGL2', $sglLibDir);
-Uber_Loader::registerNamespace('HTML', $sglLibDir);
 Uber_Loader::registerNamespace('Zend', $sglLibDir);
 Uber_Loader::registerNamespace('Horde', $sglLibDir);
+
+//set_include_path(get_include_path() . PATH_SEPARATOR . '..' .PATH_SEPARATOR . $sglLibDir);
 
 try {
     $front = new SGL2_Controller_Front();
