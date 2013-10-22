66f72fd0de454cd5d06ec1577bc8077bb758a622
Demian Turner <demian@phpkitchen.com>
2013-10-22 18:46:20 +0100

    cleanup, tests running, fixed autoload

--- www/index.php
+++ www/index.php
@@ -1,13 +1,13 @@
 <?php
-
 define('PROJECT_PATH', dirname(dirname(__FILE__)));
 
 //  setup paths
 $root = dirname(PROJECT_PATH);
 $sglLibDir = $root .'/sgl2/src/lib';
+$sglModuleDir = PROJECT_PATH . '/modules';
 
 // set include_path
-set_include_path(get_include_path() . PATH_SEPARATOR . $sglLibDir);
+set_include_path(get_include_path() . PATH_SEPARATOR . $sglLibDir . PATH_SEPARATOR . $sglModuleDir);
 
 function autoload($className)
 {
